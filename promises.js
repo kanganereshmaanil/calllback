88ed2fb941f4cf575fd9fe31265c77b4359fef63
Kanganereshma <Kanganereshma1010@gmail.com>
2022-12-24 15:06:46 +0530

    promises Async Await

--- promises.js
+++ promises.js
@@ -31,111 +31,166 @@ function calltime(){
  }
  calltime()
 
- const funA = () => {
-    setTimeout(function()  {
-        console.log(`Welcome funA`);
-        funB();
-    }, 1000);
- }
-
- const funB = () => {
-    console.log(`welcome funB`);
- }
-
- funA();
-
-
- // Create examples to explain callback hell function
-
- const getRollNo = () => {
-    setTimeout( () => {
-        console.log(`API getting roll no`);
-        let roll_no = [1,2,3,4,5];
-        console.log(roll_no);
-
-        setTimeout((rollno) => {
-            const biodata = {
-                name : 'Reshma',
-                age : 21
-            }
-            console.log(`My roll no is ${rollno}. My name is ${biodata.name} and I am ${biodata.age} years old`);
-
-            setTimeout((name) => {
-                biodata.gender = 'Female';
-                console.log(`My roll no is ${rollno}. My name is ${biodata.name} and I am ${biodata.age} years old`);
-
-            }, 1000, biodata.name);
-        }, 1000, roll_no[1]);
-
-    }, 1000);
- }
- getRollNo();
-
- //  Create examples to explain promises function
-
- new Promise ((resolveOuter) =>
-   {
-    resolveOuter(
-        new Promise((resolveInner) =>
-         {
-            setTimeout((resolveInner) => {
-                
-            }, 1000);
-         })
-    );
-   }
- );
-
- const promiseA = new Promise((resolve, reject) => {
-    resolve(777);
-  });
-  // At this point, "promiseA" is already settled.
-  promiseA.then((val) => console.log("asynchronous logging has val:", val));
-  console.log("immediate logging");
  
 
+ //Q3 "Write promise function to print numbers from 1 to 7, in which 1 should be printed after 1 sec , 2 should be printed after 2 sec, 3 should be printed after 3 sec and so on. 
 
-  // Create examples to explain async await function
-  async function f() {
-    console.log('Async function.');
-    return Promise.resolve(1);
+// Numbers
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7"
+let prntnumber = (tme, num) => {
+    return new Promise((res, rej) => {
+        if(num){
+            setTimeout(()=>{
+                res(num());
+            }, tme)
+        }
+        else{
+            rej(console.log("No Number"))
+        }
+
+    })
 }
 
-f().then(function(result) {
-    console.log(result)
-});
+prntnumber(0, ()=>console.log("Number"))
+.then(()=>{
+    prntnumber(1000, ()=>console.log("1"))
+})
+.then(()=>{
+    prntnumber(2000, ()=>console.log("2"))
+})
+.then(()=>{
+    prntnumber(3000, ()=>console.log("3"))
+})
+.then(()=>{
+    prntnumber(4000, ()=>console.log("4"))
+})
+.then(()=>{
+    prntnumber(5000, ()=>console.log("5"))
+})
+.then(()=>{
+    prntnumber(6000, ()=>console.log("6"))
+})
+.then(()=>{
+    prntnumber(7000, ()=>console.log("7"))
+})
+
+//Q4 Create a promise function accepting a argument, if yes is passed to the function then it should go to 
+// resolved state and print Promise Resolved, and if nothing is passed then it should go to reject state and 
+// catch the error and print Promise Rejected 
+
+let x = function(condition){
+    return new Promise((resolve, reject)=>{
+        if(condition == 'yes'){
+            resolve('promise resolved')
+        }
+        else{
+            reject('promise rejected')
+        }
+    })
+}
+x('yes').then(response => console.log(response)).catch(err=>console.log(err))
 
 
-// Second Example
-// a promise
-let promise = new Promise(function (resolve, reject) {
-    setTimeout(function () {
-    resolve('Promise resolved')}, 4000); 
-});
-// async function
-async function asyncFunc() {
+//Q5 Create examples to explain callback function
 
-    // wait until the promise resolves 
-    let result = await promise; 
+function fun(a, b, add) {
+    console.log(a);
+    add(2, 3);
+    console.log(b);
 
-    console.log(result);
-    console.log('hello');
 }
 
-// calling the async function
-asyncFunc();
+function add(c, d) {
+    console.log(`sum is ${c + d}`);
+}
+
+fun(2, 3, add)
 
 
-// Create examples to explain promise.all function
-const promise1 = Promise.resolve(3);
-const promise2 = 42;
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, 'foo');
-});
+//Q6 Create examples to explain callback hell function
 
-Promise.all([promise1, promise2, promise3]).then((values) => {
-  console.log(values);
-});
+function CallBackDemo() {
+    CallBackDemo1()
+    function CallBackDemo1() {
+        CallBackDemo2()
+        function CallBackDemo2() {
+            CallBackDemo3()
+            function CallBackDemo3() {
+                CallBackDemo4()
+                function CallBackDemo4() {
+                    console.log("Callback Functon");
+                }
+            }
+        }
+    }
+}
+CallBackDemo()
+
+//Q7 Create examples to explain promises function
+
+let message = (num) => {
+    return new Promise((res, rej) => {
+        if(num){
+            res(num())
+        }
+        else{
+            rej(console.log("No Data"))
+        }
+
+    })
+}
+
+message(()=>console.log("Message is printed"))
+.then(()=>{
+    prntdata(()=>console.log("Promise Functon"))
+})
+.catch(()=>console.log("No Data available"))
 
 
+//Q8 Create examples to explain async await function
 
+
+let vari=5;
+console.log(vari);
+add1();
+console.log("Hello");
+function add1(){
+    setTimeout(()=>{
+        console.log("Async Await Called");
+
+    }, 2000)
+}
+
+//Q10 Create examples to explain promise.all function
+
+const fun1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('The first promise has resolved');
+      resolve(10);
+    },1000);
+  });
+  const fun2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('The second promise has resolved');
+      resolve(20);
+    }, 2000);
+  });
+  const fun3 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('The third promise has resolved');
+      resolve(30);
+    }, 3000);
+  });
+  
+  Promise.all([fun1, fun2, fun3]).then((results) => {
+    const total = results.reduce((p, c) => p + c);
+  
+    console.log(`Results: ${results}`);
+    console.log(`Total: ${total}`);
+  });
